43f81489f3a951ccdd6ccebdd1390fd9a21dd2bc
Tushar Ghosh <tg304429@gmail.com>
2026-08-24 09:53:06 +0530

    fix-port-env-var

--- app.py
+++ app.py
@@ -240,6 +240,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="127.0.0.1",
-        port=8000,
+        port=int(os.getenv("PORT", "8080")),
         reload=True,
     )
